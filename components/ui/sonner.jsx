121488c1131adcec447142aaa6c7b97646c84edd
Aditya Raul <raul.aditya912@gmail.com>
2026-06-18 08:57:17 +0530

    updated error toast's cross icon to triangle alert

--- components/ui/sonner.jsx
+++ components/ui/sonner.jsx
@@ -4,7 +4,6 @@ import {
   CircleCheckIcon,
   InfoIcon,
   Loader2Icon,
-  OctagonXIcon,
   TriangleAlertIcon,
 } from "lucide-react"
 import { useTheme } from "next-themes"
@@ -23,7 +22,7 @@ const Toaster = ({
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
         warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
+        error: <TriangleAlertIcon className="size-4" />,
         loading: <Loader2Icon className="size-4 animate-spin" />,
       }}
       style={{
